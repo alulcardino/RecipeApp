7da9c3026b50fe54c9105343575b116848f06f6f
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-10 14:17:58 +0600

    button navigation is added

--- app/src/main/java/com/romanmikhailenko/recipeapp/MainActivity.kt
+++ app/src/main/java/com/romanmikhailenko/recipeapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.romanmikhailenko.recipeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.romanmikhailenko.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
     }
 }

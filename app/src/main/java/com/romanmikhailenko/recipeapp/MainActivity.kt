9de6003445e7e7615e12bed4486ce448e8ba4010
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-12 23:30:51 +0600

    RA-04 supportFragmentManager is added

--- app/src/main/java/com/romanmikhailenko/recipeapp/MainActivity.kt
+++ app/src/main/java/com/romanmikhailenko/recipeapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.romanmikhailenko.recipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.romanmikhailenko.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +12,14 @@ class MainActivity : AppCompatActivity() {
     private val mBinding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val fragmentTransaction: android.app.FragmentTransaction? = fragmentManager.beginTransaction()
-        fragmentTransaction?.commit()
-        _binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
+        val fragment: Fragment = CategoriesListFragment()
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction()
+            .add(R.id.mainContainer, fragment)
+            .commit()
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
     }
 }

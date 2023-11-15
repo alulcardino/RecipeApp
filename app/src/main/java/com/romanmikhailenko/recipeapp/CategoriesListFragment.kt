8ed70dbcad9a8d0a5fcee36eb4924ec3181c1b72
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-16 02:37:39 +0600

    RA-06 models for db is added

--- app/src/main/java/com/romanmikhailenko/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/romanmikhailenko/recipeapp/CategoriesListFragment.kt
@@ -6,12 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.romanmikhailenko.recipeapp.databinding.FragmentCategoriesListBinding
+import com.romanmikhailenko.recipeapp.model.Category
 
 
 class CategoriesListFragment : Fragment() {
 
     private var _binding: FragmentCategoriesListBinding? = null
     private val mBinding get() = _binding!!
+    private val listOfCategory: List<Category> = listOf(
+        Category("Рецепты всех популярных видов бургеров", 0, "burger.png", "Бургеры"),
+        Category("Самые вкусные рецепты десертов специально для вас", 1, "dessert.png", "Десерты"),
+        Category("Пицца на любой вкус и цвет. Лучшая подборка для тебя", 2, "pizza.png", "Пицца"),
+        Category("Печеная, жареная, сушеная, любая рыба на твой вкус", 3, "fish.png", "Рыба"),
+        Category("От классики до экзотики: мир в одной тарелке", 4, "soup.png", "Супы"),
+        Category("Хрустящий калейдоскоп под соусом вдохновения", 5, "salad.png", "Салаты"),
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
---
+++ app/src/main/java/com/romanmikhailenko/recipeapp/model/Category.kt
@@ -0,0 +1,8 @@
+package com.romanmikhailenko.recipeapp.model
+
+data class Category(
+    val description: String,
+    val id: Int,
+    val imageUrl: String,
+    val title: String
+)

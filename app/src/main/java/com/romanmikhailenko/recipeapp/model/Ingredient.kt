27b15d4a4678a28a20eb6a0a0f78ebe5a905a966
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-12-05 21:50:26 +0600

    RA-12 parceble recipe is added

--- app/src/main/java/com/romanmikhailenko/recipeapp/model/Ingredient.kt
+++ app/src/main/java/com/romanmikhailenko/recipeapp/model/Ingredient.kt
@@ -1,7 +1,11 @@
 package com.romanmikhailenko.recipeapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Ingredient(
     val description: String,
     val quantity: String,
     val unitOfMeasure: String
-)
+) : Parcelable
--- app/src/main/java/com/romanmikhailenko/recipeapp/model/Recipe.kt
+++ app/src/main/java/com/romanmikhailenko/recipeapp/model/Recipe.kt
@@ -1,9 +1,13 @@
 package com.romanmikhailenko.recipeapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Recipe(
     val id: Int,
     val imageUrl: String,
     val ingredients: List<Ingredient>,
     val method: List<String>,
     val title: String
-)
+) : Parcelable

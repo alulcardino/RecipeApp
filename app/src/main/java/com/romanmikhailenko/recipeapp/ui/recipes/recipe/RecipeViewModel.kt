4076e921cb29174f1884fdabaa2d275147ad70d8
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-12-14 23:16:31 +0600

    RA-20 recipestate is changed

--- app/src/main/java/com/romanmikhailenko/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/romanmikhailenko/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -2,13 +2,12 @@ package com.romanmikhailenko.recipeapp.ui.recipes.recipe
 
 import androidx.lifecycle.ViewModel
 import com.romanmikhailenko.recipeapp.model.Ingredient
+import com.romanmikhailenko.recipeapp.model.Recipe
 
 data class RecipeState(
-    var id: Int = 0,
-    var imageUrl: String = "",
-    var ingredients: List<Ingredient> = listOf(),
-    var method: List<String> = listOf(),
-    var title: String = "Recipe"
+    var recipe: Recipe? = null,
+    var portions: Int = 1,
+    var isFavorite: Boolean = false
 )
 
 class RecipeViewModel : ViewModel() {
